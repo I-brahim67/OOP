ed5569649209d3a5725cc271f080ea3377c99639
I-brahim67 <ibrahim.1freefire@gmail.com>
2026-03-07 15:45:41 +0500

    done using file handeling

--- ConstructorFriend.cpp
+++ ConstructorFriend.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
-#include<cmath>
-#include <iomanip>
+#include <cmath>
 using namespace std;
-class Point{
-int x,y;
+class Point
+{
+    int x, y;
+
 public:
-friend void Distance(Point o1,Point o2);
-Point(int a,int b){
-x=a;
-y=b;
-}
-void display(){
-cout<<"the point is ( "<<x<<" , "<<y<<" ) "<<endl;
-}
+    friend void Distance(Point o1, Point o2);
+    Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+    void display()
+    {
+        cout << "the point is ( " << x << " , " << y << " ) " << endl;
+    }
 };
-void Distance(Point o1,Point o2){
-float dist;
-dist = sqrt(pow(o2.x-o1.x,2)+pow(o2.y-o1.y,2));
-cout<<"Distance is: "<<dist<<endl;
+void Distance(Point o1, Point o2)
+{
+    float dist;
+    dist = sqrt(pow(o2.x - o1.x, 2) + pow(o2.y - o1.y, 2));
+    cout << "Distance is: " << dist << endl;
 }
-int main() {
-    Point o1(2,3);
+int main()
+{
+    Point o1(2, 3);
     o1.display();
-    Point o2(3,5);
+    Point o2(3, 5);
     o2.display();
-    Distance(o1,o2);
+    Distance(o1, o2);
     return 0;
 }
--- PassingClassValueToFunc.cpp
+++ PassingClassValueToFunc.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <iomanip>
+#include<fstream>
 using namespace std;
 class Complex{
 int a,b;
 public:
 void SetData(int V1,int V2);
 void SetDataBySum(Complex o1,Complex o2);
-void GetData();
+void SetDataByProduct(Complex o1,Complex o2);
+void SetDataByDifference(Complex o1,Complex o2);
+void GetData(ofstream &fout);
 };
 void Complex :: SetData(int V1,int V2){
 a=V1;
@@ -16,19 +19,41 @@ void Complex :: SetDataBySum(Complex o1,Complex o2){
 a = o1.a + o2.a;
 b = o1.b + o2.b;
 }
-void Complex :: GetData(){
-cout<<"Your complex number is: "<<a<<" + "<<b<<"i"<<endl;
+void Complex :: SetDataByProduct(Complex o1,Complex o2){
+a = o1.a * o2.a - o1.b * o2.b;
+b = o1.a * o2.b + o1.b * o2.a;
+}
+void Complex :: SetDataByDifference(Complex o1,Complex o2){
+a = o1.a - o2.a;
+b = o1.b - o2.b;
+}
+void Complex :: GetData(ofstream &fout){
+fout<<"Your complex number is: "<<a<<" + "<<b<<"i"<<endl;
 }
 
 int main() {
+    ifstream fin("input.txt");
+    if (!fin) {
+        cout << "Error opening file!" << endl;
+    }
+    ofstream fout("output.txt");
+    int a,b;
+    fin>>a>>b;
     Complex c1,c2,c3;
-    c1.SetData(1,2);
-    c1.GetData();
-
-    c2.SetData(3,4);
-    c2.GetData();
+    c1.SetData(a,b);
+    c1.GetData(fout);
+    fin>>a>>b;
+    c2.SetData(a,b);
+    c2.GetData(fout);
 
     c3.SetDataBySum(c1,c2);
-    c3.GetData();
+    c3.GetData(fout);
+    c3.SetDataByProduct(c1,c2);
+    c3.GetData(fout);
+
+    c3.SetDataByDifference(c1,c2);
+    c3.GetData(fout);
+    fin.close();
+    fout.close();
     return 0;
 }
